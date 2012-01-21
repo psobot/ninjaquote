56c3b45b97e5825eb0c3dc4c888987ad29a17e55
Peter Sobot <psobot@gmail.com>
2012-01-20 23:49:48 -0500

    Made UI a little better. Ish.

--- server/public/client.js
+++ server/public/client.js
@@ -1,3 +1,11 @@
+fbimg = function(id) {
+  return "<img src='http://graph.facebook.com/" + id + "/picture?type=large' />";
+}
+
+header = function(bool) {
+  return bool ? "Yup!" : "Nope!";
+}
+
 window.fbAsyncInit = function() {
     FB.init({
         appId : '330392253659950',
@@ -20,7 +28,8 @@ window.fbAsyncInit = function() {
 }(document));
 
 $(function(){
-  $('.login').click(function(){
+  $('.fb-login-button').click(function(){
+    $('#header').slideUp();
     FB.login(function(response) {
       var spinner = new Spinner({
         lines: 12, // The number of lines to draw
@@ -35,14 +44,50 @@ $(function(){
       $("#loading_spinner").slideDown();
 
       if (response.authResponse) {
-          $.getJSON('get_entry', {token : FB.getAccessToken() }, function(data) {
-              console.log(data);
-              $('#loading_spinner').slideUp();
-              $('#question').slideDown();
+        $.getJSON('get_entry', {token : FB.getAccessToken() }, function(data) {
+          console.log(data);
+
+          people = $('#people_wrapper a.person');
+          person1div = $(people[0]);
+          person2div = $(people[1]);
+
+          $(".front h3", person1div).html(data.friend1.first_name);
+          $(".front h3", person2div).html(data.friend2.first_name);
+
+          $(".front .image_128", person1div).html(fbimg(data.friend1.id));
+          $(".front .image_128", person2div).html(fbimg(data.friend2.id));
+
+          $(".front .image_128", person1div).html(fbimg(data.friend1.id));
+
+          $(person1div).addClass(data.quote.from.id == data.friend1.id ? 'yes' : 'no');
+          $(person2div).addClass(data.quote.from.id == data.friend2.id ? 'yes' : 'no');
+
+          $(".back h3", person1div).html(header(data.quote.from.id == data.friend1.id));
+          $(".back h3", person2div).html(header(data.quote.from.id == data.friend2.id));
+
+          $(".back p.first", person1div).html(function(){
+            if (data.quote.from.id == data.friend1.id) return data.friend1.first_name + " said that!";
+            else return data.friend1.first_name + " didn't say that.";
           });
-      }
-      else {
-          console.log('User cancelled login or did not fully authorize.');
+          $(".back p.first", person2div).html(function(){
+            if (data.quote.from.id == data.friend2.id) return data.friend2.first_name + " said that!";
+            else return data.friend2.first_name + " didn't say that.";
+          });
+
+          $(".back p.wrong", person1div).html(function(){
+            return "It was " + data.friend1.first_name + "!";
+          });
+          $(".back p.wrong", person2div).html(function(){
+            return "It was " + data.friend2.first_name + "!";
+          });
+
+          $('#quote p').html(data.quote.message);
+
+          $('#loading_spinner').slideUp();
+          $('#question').slideDown();
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
       }
     }, {scope: 'read_friendlists,friends_status'});
   });
