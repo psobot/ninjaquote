3a6c6145b313cb98ed6e41efef1a9f88c4ef44e2
Scott Greenlay <sgreenlay@gmail.com>
2012-01-21 05:49:18 -0500

    server - opengraph api optimizations/refactoring

--- server/server.js
+++ server/server.js
@@ -3,37 +3,12 @@
  * server.js
  */
 
-var https = require('https');
 var express = require('express');
+var opengraph = require('./opengraph');
 
 var app = express.createServer();
 
-var page_size = 50;
-
-var graph_api = function(path, callback) {
-    var options = {
-        host: 'graph.facebook.com',
-        port: 443,
-        path: path,
-        method: 'GET'
-    };
-    
-    var req = https.request(options, function(res) {
-        var data = '';
-        
-        res.on('data', function(chunk) {
-            data += chunk;
-        });
-        res.on('end', function() {
-            callback(JSON.parse(data));
-        });
-    });
-    req.end();
-    
-    req.on('error', function(e) {
-        callback(null);
-    });
-};
+var page_size = 250;
 
 app.configure(function(){
     app.use(express.methodOverride());
@@ -42,80 +17,62 @@ app.configure(function(){
     app.use(app.router);
 });
 
-app.get('/', function(req, res) {
-    res.redirect("/index.html");
-});
-
-var get_random_friend = function(token, probability, page, callback) {
-    var path = '/me/friends?access_token=' + token + '&limit=' + page_size  + '&offset=' + (page * page_size);
-    graph_api(path, function (ret) {
-        if (ret == null) {
-            callback(null);
+var get_random = function(type, token, probability, page, callback, error) {
+    var path = '/' + type + '?access_token=' + token + '&limit=' + page_size  + '&offset=' + (page * page_size);
+    opengraph.api(path, function (ret) {
+        if ((ret == null) || (ret.data.length == 0)) {
+            error();
         }
         else if (ret.data.length == 0) {
-            get_random_friend(token, 1.0, Math.floor(Math.random() * page), callback); 
+            get_random(type, token, 1.0, Math.floor(Math.random() * page), callback, error); 
         }
         else if (Math.random() > probability) {
-            get_random_friend(token, (probability * 0.5), page + 1, callback); 
+            get_random(type, token, (probability * 0.5), page + 1, callback, function () {
+                callback(ret.data[Math.floor(Math.random() * ret.data.length)]);
+            }); 
         }
         else {
-            var offset = Math.floor(Math.random() * ret.data.length);
-            var path = '/' + ret.data[offset].id + '?access_token=' + token;
-            graph_api(path, function (ret) {
-                callback(ret);
-            });
+            callback(ret.data[Math.floor(Math.random() * ret.data.length)]);
         }
     });
 };
 
-var get_random_quote = function(token, id, probability, page, callback) {
-    var path = '/' + id + '/statuses?access_token=' + token + '&limit=' + page_size + '&offset=' + (page * page_size);
-    graph_api(path, function (ret) {
-        if (ret == null) {
-            callback(null);
-        }
-        else if (ret.data.length == 0) {
-            get_random_quote(token, id, 1.0, Math.floor(Math.random() * page), callback); 
-        }
-        else if (Math.random() > probability) {
-            get_random_quote(token, id, (probability * 0.5), page + 1, callback); 
-        }
-        else {
-            var offset = Math.floor(Math.random() * ret.data.length);
-            callback(ret.data[offset]);
-        }
-    });
+var get_random_friend = function(token, callback, error) {
+    get_random('me/friends', token, 0.5, 0, function(person_entry) {
+        var path = '/' + person_entry.id + '?access_token=' + token;
+        opengraph.api(path, function (person) {
+            callback(person);
+        });
+    }, error);
 };
 
-var get_entry = function(token, callback) {
-    get_random_friend(token, 0.5, 0, function (person) {
-        if (person == null) {
-            console.log("retry - bad person");
-            get_entry(token, callback);
-        }
-        else {
-            var friend1 = person;
-            get_random_friend(token, 0.5, 0, function (person) {
-                if ((person == null) || (person.id == friend1.id)) {
-                    console.log("retry - bad person");
-                    get_entry(token, callback);
-                }
-                else {
-                    var friend2 = person;
-                    var chosen_one = (Math.floor(Math.random() * 2) == 0) ? friend1 : friend2;
-                    
-                    get_random_quote(token, chosen_one.id, 0.5, 0, function (quote) {
-                        if ((quote == undefined) || (quote == null)) {
-                            console.log("retry - bad quote");
-                            get_entry(token, callback);
-                        }
-                        else {
-                            callback(friend1, friend2, quote);
-                        }
-                    });
-                }
-            });
-        }
+var get_random_quote = function(token, id, callback, error) {
+    get_random(id + '/statuses', token, 0.5, 0, callback, error);
+};
+
+var get_entry = function(token, callback, error) {
+    get_random_friend(token, function (friend1) {
+        get_random_friend(token, function (friend2) {
+            if (friend1.id == friend2.id) {
+                console.log("retry - duplicate friend 2");
+                get_entry(token, callback, error);
+            }
+            else {
+                var chosen_one = (Math.floor(Math.random() * 2) == 0) ? friend1 : friend2;
+                get_random_quote(token, chosen_one.id, function (quote) {
+                    callback(friend1, friend2, quote);
+                }, function() {
+                    console.log("retry - bad quote");
+                    get_entry(token, callback, error);
+                });
+            }
+        }, function() {
+            console.log("retry - bad friend 2");
+            get_entry(token, callback, error);
+        });
+    }, function() {
+        console.log("retry - bad friend 1");
+        get_entry(token, callback, error);
     });
 };
 
